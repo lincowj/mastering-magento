239c58d203bb7c777eff4d043cb67d0cead701e6
lincowj <lincoln.souza@webjump.com.br>
2021-08-06 16:32:56 -0300

    fourth section completed

--- app/code/Mastering/SampleModule/Model/Item.php
+++ app/code/Mastering/SampleModule/Model/Item.php
@@ -1,11 +1,11 @@
 <?php
 namespace Mastering\SampleModule\Model;
 
-use Magento\Framework\DataObject\IdentityInterface;
 use Magento\Framework\Model\AbstractModel;
 
 class Item extends AbstractModel
 {
+    protected $_eventPrefix = 'mastering_sample_item';
     /**
      * Initialize resource model
      *
